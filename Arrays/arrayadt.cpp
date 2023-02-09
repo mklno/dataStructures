730df080680af216f3054b25d9bb212b888d6223
Manoprasanth MATHAVAN <manodreuel@gmail.com>
2023-02-10 00:14:34 +0530

    array-adt

--- Arrays/arrayadt.cpp
+++ Arrays/arrayadt.cpp
@@ -13,6 +13,17 @@ struct arrayadt
     void append(int num);
     void insert(int index, int num);
     void delet(int index);
+    int linearSearch(int key); //returns the index of the given number
+    int binarySearch(int key); //valid only for sorted arr
+    int rBinarySearch(int key, int low, int high); //valid only for sorted arr
+    int get(int index);
+    void set(int index, int key);
+    int max();
+    int min();
+    int sum();
+    int recursiveSum(int *A, int length);
+    float average();
+    int* reverse();
 };
 // display all th elements in arr
     void arrayadt::display(){
@@ -49,11 +60,109 @@ struct arrayadt
         }
         
     }
+// returns the index of the given number if present
+    int arrayadt::linearSearch(int key){  
+        for(int i=0;i<length;i++){
+            if (A[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+// returns the index of the given number if present efficiently
+    int arrayadt::binarySearch(int key){
+        int low = 0;
+        int high = length-1;
+        int mid = (low+high)/2;
+        while(low <= high){
+            if (key < A[mid]){
+            high = mid -1;
+        }
+        else if (key > A[mid]){
+            low = mid +1;
+        }
+        else{
+            return A[mid];
+        }
+        }        
+        return -1;
+    }
+// returns the index of the given number if present using recursion
+    int arrayadt::rBinarySearch(int key, int low, int high){
+        if(low <= high){
+            int mid = (low+high)/2;
+            if (key == A[mid]){
+                return mid;
+            }else if(key < A[mid]){
+                return rBinarySearch(key, low, mid-1);
+            }else{
+                return rBinarySearch(key, mid+1, high);
+            }
+       }
+       return -1;
+    }
+// returns the element at the given given index if the index is present
+    int arrayadt::get(int index){
+        if (index < length && index > 0){
+            return A[index];
+        }
+        return -1; 
+    }
+// replace the value of the element at the given index
+    void arrayadt::set(int index, int key){
+        if (index > 0 && index < length){
+            A[index] = key;
+        }   
+    }
+// returns the max element in the arr
+    int arrayadt::max(){
+        int max = A[0];
+        for(int i=0;i < length; i++){
+            if (A[i] > max){
+                max = A[i];
+            }
+        }
+        return max;
+    }
+// returns the min element in the arr
+    int arrayadt::min(){
+        int min = A[0];
+        for(int i = 0; i < length; i++){
+            if (A[i] < min ){
+                min = A[i];
+            }
+        }
+        return min;
+    }
+// returns the total sum of all the elements in the arr
+    int arrayadt::sum(){
+        int sum = 0;
+        for (int i =0; i < length; i++){
+            sum += A[i];
+        }
+        return sum;
+    }
+// returns the total sum of all the elements in the arr using recursion
+    int arrayadt::recursiveSum(int *A,int length){
+        int n = length -1;
+        if (length < 0){
+            return 0;
+        }
+        else{
+            return arrayadt::recursiveSum(A, n-1) + A[n];
+        }
+    }
+// returns the average of the arr
+    float arrayadt::average(){
+        int sum = arrayadt::sum();
+        int average = sum/length;
+        return average;
+    }
 
 int main(){
     arrayadt arr;
     int i, n;
-
+  
     printf("Enter the size of the array\n");
     scanf("%d", &arr.size);
     arr.A = (int *)malloc(arr.size*sizeof(int)); 
@@ -75,5 +184,7 @@ int main(){
     arr.display();
     arr.delet(3);
     arr.display();
+    printf("%d\n", arr.linearSearch(5));
+    printf("%d\n", arr.binarySearch(3));
     return 0;
 }
