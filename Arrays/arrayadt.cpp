045a6fee67275dc7fcf9821cff9c5fb55c60eda0
Manoprasanth MATHAVAN <manodreuel@gmail.com>
2023-02-14 23:49:21 +0530

    arrayadt-completed

--- Arrays/arrayadt.cpp
+++ Arrays/arrayadt.cpp
@@ -1,190 +1,381 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
 using namespace std;
-
-struct arrayadt
+class Array
 {
-    /* data */
+private:
     int *A;
     int size;
     int length;
+    void swap(int *x, int *y);
 
-    void display();
-    void append(int num);
-    void insert(int index, int num);
-    void delet(int index);
-    int linearSearch(int key); //returns the index of the given number
-    int binarySearch(int key); //valid only for sorted arr
-    int rBinarySearch(int key, int low, int high); //valid only for sorted arr
-    int get(int index);
-    void set(int index, int key);
-    int max();
-    int min();
-    int sum();
-    int recursiveSum(int *A, int length);
-    float average();
-    int* reverse();
+public:
+    Array()
+    {
+        size = 10;
+        length = 0;
+        A = new int[size];
+    }
+    Array(int sz)
+    {
+        size = sz;
+        length = 0;
+        A = new int[size];
+    }
+    ~Array()
+    {
+        delete[] A;
+    }
+    void Display();
+    void Append(int x);
+    void Insert(int index, int x);
+    int Delete(int index);
+    int LinearSearch(int key);
+    int BinarySearch(int key);
+    int Get(int index);
+    void Set(int index, int x);
+    int Max();
+    int Min();
+    int Sum();
+    float Avg();
+    void Reverse();
+    void Reverse2();
+    void InsertSort(int x);
+    int isSorted();
+    void Rearrange();
+    Array *Merge(Array arr2);
+    Array *Union(Array arr2);
+    Array *Diff(Array arr2);
+    Array *Inter(Array arr2);
 };
-// display all th elements in arr
-    void arrayadt::display(){
+void Array::Display()
+{
     int i;
-    printf("Element in the arr are \n"); 
-    for(i=0; i<length;i++){
-        cout<<A[i]<<endl;
-    }
+    cout << "\nElements are\n";
+    for (i = 0; i < length; i++)
+        cout << A[i] << " ";
 }
-// add an element at the end of the arr
-    void arrayadt::append(int num){
-        if (length < size){
-            A[length] = num;
-            length++;
-        }
-    }
-// insert an element at a given index
-    void arrayadt::insert(int index, int num){
-        if (length < size){
-        for(int j = length; j>index; j--){
-            A[j] = A[j-1];
-        }
-        A[index] = num;
+void Array::Append(int x)
+{
+    if (length < size)
+        A[length++] = x;
+}
+void Array::Insert(int index, int x)
+{
+    int i;
+    if (index >= 0 && index <= length)
+    {
+        for (i = length; i > index; i--)
+            A[i] = A[i - 1];
+        A[index] = x;
         length++;
     }
 }
-// delete an element at a given index
-    void arrayadt::delet(int index){
-        if (index > 0 && index < length){
-            for(int i = index; i < length-1; i++){
-            A[i]= A[i+1];
-        }
+int Array::Delete(int index)
+{
+    int x = 0;
+    int i;
+
+    if (index >= 0 && index < length)
+    {
+        x = A[index];
+        for (i = index; i < length - 1; i++)
+            A[i] = A[i + 1];
         length--;
-        }
-        
+        return x;
     }
-// returns the index of the given number if present
-    int arrayadt::linearSearch(int key){  
-        for(int i=0;i<length;i++){
-            if (A[i] == key){
-                return i;
-            }
+
+    return 0;
+}
+void Array::swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int Array::LinearSearch(int key)
+{
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        if (key == A[i])
+        {
+            swap(&A[i], &A[0]);
+            return i;
         }
-        return -1;
     }
-// returns the index of the given number if present efficiently
-    int arrayadt::binarySearch(int key){
-        int low = 0;
-        int high = length-1;
-        int mid = (low+high)/2;
-        while(low <= high){
-            if (key < A[mid]){
-            high = mid -1;
-        }
-        else if (key > A[mid]){
-            low = mid +1;
-        }
-        else{
-            return A[mid];
-        }
-        }        
-        return -1;
+    return -1;
+}
+int Array::BinarySearch(int key)
+{
+    int l, mid, h;
+    l = 0;
+    h = length - 1;
+
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (key == A[mid])
+            return mid;
+        else if (key < A[mid])
+            h = mid - 1;
+        else
+            l = mid + 1;
     }
-// returns the index of the given number if present using recursion
-    int arrayadt::rBinarySearch(int key, int low, int high){
-        if(low <= high){
-            int mid = (low+high)/2;
-            if (key == A[mid]){
-                return mid;
-            }else if(key < A[mid]){
-                return rBinarySearch(key, low, mid-1);
-            }else{
-                return rBinarySearch(key, mid+1, high);
-            }
-       }
-       return -1;
+    return -1;
+}
+int Array::Get(int index)
+{
+    if (index >= 0 && index < length)
+        return A[index];
+    return -1;
+}
+void Array::Set(int index, int x)
+{
+    if (index >= 0 && index < length)
+        A[index] = x;
+}
+int Array::Max()
+{
+    int max = A[0];
+    int i;
+    for (i = 1; i < length; i++)
+    {
+        if (A[i] > max)
+            max = A[i];
     }
-// returns the element at the given given index if the index is present
-    int arrayadt::get(int index){
-        if (index < length && index > 0){
-            return A[index];
-        }
-        return -1; 
+    return max;
+}
+int Array::Min()
+{
+    int min = A[0];
+    int i;
+    for (i = 1; i < length; i++)
+    {
+        if (A[i] < min)
+            min = A[i];
     }
-// replace the value of the element at the given index
-    void arrayadt::set(int index, int key){
-        if (index > 0 && index < length){
-            A[index] = key;
-        }   
+    return min;
+}
+int Array::Sum()
+{
+    int s = 0;
+    int i;
+    for (i = 0; i < length; i++)
+        s += A[i];
+
+    return s;
+}
+float Array::Avg()
+{
+    return (float)Sum() / length;
+}
+void Array::Reverse()
+{
+    int *B;
+    int i, j;
+
+    B = (int *)malloc(length * sizeof(int));
+    for (i = length - 1, j = 0; i >= 0; i--, j++)
+        B[j] = A[i];
+    for (i = 0; i < length; i++)
+        A[i] = B[i];
+}
+void Array::Reverse2()
+{
+    int i, j;
+    for (i = 0, j = length - 1; i < j; i++, j--)
+    {
+        swap(&A[i], &A[j]);
     }
-// returns the max element in the arr
-    int arrayadt::max(){
-        int max = A[0];
-        for(int i=0;i < length; i++){
-            if (A[i] > max){
-                max = A[i];
-            }
-        }
-        return max;
+}
+void Array::InsertSort(int x)
+{
+    int i = length - 1;
+    if (length == size)
+        return;
+    while (i >= 0 && A[i] > x)
+    {
+        A[i + 1] = A[i];
+        i--;
     }
-// returns the min element in the arr
-    int arrayadt::min(){
-        int min = A[0];
-        for(int i = 0; i < length; i++){
-            if (A[i] < min ){
-                min = A[i];
-            }
-        }
-        return min;
-    }
-// returns the total sum of all the elements in the arr
-    int arrayadt::sum(){
-        int sum = 0;
-        for (int i =0; i < length; i++){
-            sum += A[i];
-        }
-        return sum;
-    }
-// returns the total sum of all the elements in the arr using recursion
-    int arrayadt::recursiveSum(int *A,int length){
-        int n = length -1;
-        if (length < 0){
+    A[i + 1] = x;
+    length++;
+}
+int Array::isSorted()
+{
+    int i;
+    for (i = 0; i < length - 1; i++)
+    {
+        if (A[i] > A[i + 1])
             return 0;
+    }
+    return 1;
+}
+void Array::Rearrange()
+{
+    int i, j;
+    i = 0;
+    j = length - 1;
+
+    while (i < j)
+    {
+        while (A[i] < 0)
+            i++;
+        while (A[j] >= 0)
+            j--;
+        if (i < j)
+            swap(&A[i], &A[j]);
+    }
+}
+Array *Array::Merge(Array arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    Array *arr3 = new Array(length + arr2.length);
+
+    while (i < length && j < arr2.length)
+    {
+        if (A[i] < arr2.A[j])
+            arr3->A[k++] = A[i++];
+        else
+            arr3->A[k++] = arr2.A[j++];
+    }
+    for (; i < length; i++)
+        arr3->A[k++] = A[i];
+    for (; j < arr2.length; j++)
+        arr3->A[k++] = arr2.A[j];
+    arr3->length = length + arr2.length;
+
+    return arr3;
+}
+Array *Array::Union(Array arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    Array *arr3 = new Array(length + arr2.length);
+
+    while (i < length && j < arr2.length)
+    {
+        if (A[i] < arr2.A[j])
+            arr3->A[k++] = A[i++];
+        else if (arr2.A[j] < A[i])
+            arr3->A[k++] = arr2.A[j++];
+        else
+        {
+            arr3->A[k++] = A[i++];
+            j++;
         }
-        else{
-            return arrayadt::recursiveSum(A, n-1) + A[n];
+    }
+    for (; i < length; i++)
+        arr3->A[k++] = A[i];
+    for (; j < arr2.length; j++)
+        arr3->A[k++] = arr2.A[j];
+
+    arr3->length = k;
+
+    return arr3;
+}
+Array *Array::Inter(Array arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    Array *arr3 = new Array(length + arr2.length);
+
+    while (i < length && j < arr2.length)
+    {
+        if (A[i] < arr2.A[j])
+            i++;
+        else if (arr2.A[j] < A[i])
+            j++;
+        else if (A[i] == arr2.A[j])
+        {
+            arr3->A[k++] = A[i++];
+            j++;
         }
     }
-// returns the average of the arr
-    float arrayadt::average(){
-        int sum = arrayadt::sum();
-        int average = sum/length;
-        return average;
+
+    arr3->length = k;
+
+    return arr3;
+}
+Array *Array::Diff(Array arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    Array *arr3 = new Array(length + arr2.length);
+
+    while (i < length && j < arr2.length)
+    {
+        if (A[i] < arr2.A[j])
+            arr3->A[k++] = A[i++];
+        else if (arr2.A[j] < A[i])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
     }
+    for (; i < length; i++)
+        arr3->A[k++] = A[i];
 
-int main(){
-    arrayadt arr;
-    int i, n;
-  
-    printf("Enter the size of the array\n");
-    scanf("%d", &arr.size);
-    arr.A = (int *)malloc(arr.size*sizeof(int)); 
-    arr.length = 0;
+    arr3->length = k;
 
-    printf("Enter the number of numbers\n");
-    scanf("%d", &n);
-    printf("Enter the all the numbers\n");
-    // Reading all the elements of an arr
-    for(i=0; i<n; i++){
-        scanf("%d", &arr.A[i]);
-        arr.length++;
+    return arr3;
+}
+int main()
+{
+    Array *arr1;
+    int ch, sz;
+    int x, index;
 
-    }
-    arr.display(); 
-    arr.append(10);
-    arr.display();
-    arr.insert(5,6);
-    arr.display();
-    arr.delet(3);
-    arr.display();
-    printf("%d\n", arr.linearSearch(5));
-    printf("%d\n", arr.binarySearch(3));
+    cout << "Enter Size of Array";
+    scanf("%d", &sz);
+    arr1 = new Array(sz);
+
+    do
+    {
+        cout << "\n\nMenu\n";
+        cout << "1. Insert\n";
+        cout << "2. Delete\n";
+        cout << "3. Search\n";
+        cout << "4. Sum\n";
+        cout << "5. Display\n";
+        cout << "6.Exit\n";
+
+        cout << "enter you choice ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case 1:
+            cout << "Enter an element and index ";
+            cin >> x >> index;
+            arr1->Insert(index, x);
+            break;
+        case 2:
+            cout << "Enter index ";
+            cin >> index;
+            x = arr1->Delete(index);
+            cout << "Deleted Element is" << x;
+            break;
+        case 3:
+            cout << "Enter element to search ";
+            cin >> x;
+            index = arr1->LinearSearch(x);
+            cout << "Element index " << index;
+            break;
+        case 4:
+            cout << "Sum is " << arr1->Sum();
+            break;
+        case 5:
+            arr1->Display();
+        }
+    } while (ch < 6);
     return 0;
 }
